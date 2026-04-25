bf651177caa490deeeae208ecfb6c24dc3a82014
Agusteando <desarrollo.tecnologico@casitaiedis.edu.mx>
2026-04-25 15:51:31 -0600

    removed unused file

--- server/api/students/[matricula]/photo.get.ts
+++
@@ -1,7 +0,0 @@
-## server/api/students/[matricula]/photo.get.ts
-
-// Deprecated: Photo lookups are now handled directly by the client
-// against the external endpoint to minimize serverless function overhead.
-export default defineEventHandler(() => {
-  return { photoUrl: null }
-})
