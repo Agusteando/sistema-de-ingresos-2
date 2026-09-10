7a5f00503b8f20921d87df899a6548c577a47697
Alejandro Jurado <desarrollo.tecnologico@casitaiedis.edu.mx>
2026-09-10 15:14:52 -0600

    WIP catálogo

--- server/api/external/v1/talleres/roster.get.ts
+++ server/api/external/v1/talleres/roster.get.ts
@@ -1,7 +1,18 @@
 import { assertTalleresPortalAccess } from '../../../../utils/talleres-portal-auth'
 import { readTalleresSnapshotRoster } from '../../../../utils/talleres-snapshot'
+import { readCompleteTalleresExternalCatalog } from '../../../../utils/talleres-external-catalog'
 
 export default defineEventHandler(async (event) => {
   await assertTalleresPortalAccess(event)
-  return await readTalleresSnapshotRoster(getQuery(event))
+  const [snapshot, completeCatalog] = await Promise.all([
+    readTalleresSnapshotRoster(getQuery(event)),
+    readCompleteTalleresExternalCatalog(),
+  ])
+  return {
+    ...snapshot,
+    catalog: completeCatalog.catalog,
+    talleres: completeCatalog.talleres,
+    servicios: completeCatalog.servicios,
+    catalogSource: completeCatalog.source,
+  }
 })
